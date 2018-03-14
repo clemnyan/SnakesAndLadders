de75d0482f7ea8c1b72fb0cc5e46a8ab140bf354
Clement Nyanhongo <clemnyan@gmail.com>
2018-03-14 14:07:29 -0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -10,24 +10,24 @@ using namespace std;
 int main(){
 
   //Create new game instance
-	Main_Game *g1 = new Main_Game();
+  Main_Game *g1 = new Main_Game();
 
   int start = 0;
-	while (start ==0) {
-		start = g1 -> intro();    //intro populates game properties
-	}
+     while (start ==0) {
+     start = g1 -> intro();    //intro populates game properties
+   }
 
-	//Print the game board
-	g1 -> printBoard();
+    //Print the game board
+   g1 -> printBoard();
 
-  cout << g1->isComplete()<<endl;
-	while (g1 -> isComplete() == 0) {
-    g1 ->Play_Game();
-	}
+   cout << g1->isComplete()<<endl;
+   while (g1 -> isComplete() == 0) {
+      g1 ->Play_Game();
+   }
 
-	g1 -> Play_Game();  //to print winners
-	delete(g1); //destructor
+    g1 -> Play_Game();  //to print winners
+    delete(g1); //destructor
 
-	return 0;
+    return 0;
 }
 
